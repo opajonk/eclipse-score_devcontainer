7a294cf06a0fdeae68b2e1e85c7ffcd2df4a321a
Oliver Pajonk <oliver.pajonk@elektrobit.com>
2025-10-30 07:21:39 +0000

    replace devcontainer name so we can publish

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-devcontainer build --platform linux/arm64 --workspace-folder src/s-core-devcontainer --image-name ghcr.io/eclipse-score/devcontainer --cache-from ghcr.io/eclipse-score/devcontainer
-devcontainer build --platform linux/amd64 --workspace-folder src/s-core-devcontainer --image-name ghcr.io/eclipse-score/devcontainer --cache-from ghcr.io/eclipse-score/devcontainer
+devcontainer build --platform linux/arm64 --workspace-folder src/s-core-devcontainer --image-name ghcr.io/opajonk/eclipse-score_devcontainer --cache-from ghcr.io/opajonk/eclipse-score_devcontainer
+devcontainer build --platform linux/amd64 --workspace-folder src/s-core-devcontainer --image-name ghcr.io/opajonk/eclipse-score_devcontainer --cache-from ghcr.io/opajonk/eclipse-score_devcontainer
